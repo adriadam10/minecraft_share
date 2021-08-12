fdb90f0b8ba32fa0f715d19f024598e854b4610b
adriadam10 <57327458+adriadam10@users.noreply.github.com>
2021-08-12 20:18:27 +0200

    Add files via upload

--- start.bat
+++ start.bat
@@ -3,6 +3,6 @@ set /p domain="What is the domain? "
 curl https://mcsrvstat.us/server/%domain% | find /i "Could not get"
 if not errorlevel 1 (
    echo "Server is going to start"
-   duckdns.vbs
+   duckdns.ps1
    minecraft.bat
 )
