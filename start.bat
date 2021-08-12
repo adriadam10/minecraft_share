2b74edd4682c157ab844314a956d73c9c41a72b5
adriadam10 <57327458+adriadam10@users.noreply.github.com>
2021-08-12 20:40:53 +0200

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-set /p domain="What is the domain? "
+set /p domain="What is the domain? (for check if it's already online)"
 curl https://mcsrvstat.us/server/%domain% | find /i "Could not get"
 if not errorlevel 1 (
    echo "Server is going to start"
